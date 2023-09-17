fb0a47efab851f7632fedebd20f779467485dc55
nathanieladiah <nathanieladiah@gmail.com>
2023-09-17 15:55:03 -0400

    Add delete and update to the server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -35,15 +35,13 @@ export const FeedbackProvider = ({ children }) => {
     });
 
     const data = await response.json();
-
-    // not necessary with the server, is created automatically
-    // newFeedback.id = uuidv4();
-
     setFeedback([data, ...feedback]);
   };
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/api/feedback/${id}`, { method: "DELETE" });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
@@ -57,9 +55,19 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/api/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
